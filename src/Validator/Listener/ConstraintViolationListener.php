33e8e3cb99633ee60e9167531abc4bced7903156
Roman Kut'kin <16177490+romankutkin@users.noreply.github.com>
2021-12-22 12:30:09 +0700

    Update creating response in constraint violation listener

--- src/Validator/Listener/ConstraintViolationListener.php
+++ src/Validator/Listener/ConstraintViolationListener.php
@@ -4,14 +4,14 @@ declare(strict_types=1);
 
 namespace App\Validator\Listener;
 
+use App\Http\ApiResponse;
 use App\Validator\Exception\ConstraintViolationException;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 
-class ExceptionListener
+class ConstraintViloationListener
 {
-    public function onKernelException(ExceptionEvent $event)
+    public function onKernelException(ExceptionEvent $event): void
     {
         $exception = $event->getThrowable();
 
@@ -22,8 +22,6 @@ class ExceptionListener
 
     private function handleConstraintViolationException(ExceptionEvent $event, ConstraintViolationException $exception): void
     {
-        $response = new JsonResponse();
-
         $violations = $exception->getConstraintViolations();
         $errors = [];
 
@@ -37,11 +35,11 @@ class ExceptionListener
             ];
         }
 
-        $response->setStatusCode(Response::HTTP_BAD_REQUEST);
-        $response->setContent(\json_encode([
-            'errors' => $errors,
-        ]));
-
-        $event->setResponse($response);
+        $event->setResponse(ApiResponse::create(
+            status: Response::HTTP_BAD_REQUEST,
+            content: [
+                'errors' => $errors,
+            ],
+        ));
     }
 }
