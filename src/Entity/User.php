64d3b56588833327637243a9da803f6d33432e9c
Roman Kut'kin <16177490+romankutkin@users.noreply.github.com>
2021-12-23 15:30:25 +0700

    Implement UserInterface and PasswordAuthenticatedUserInterface in User entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -5,11 +5,13 @@ declare(strict_types=1);
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Uid\Uuid;
 
 #[ORM\Entity]
 #[ORM\Table(name: 'users')]
-class User
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\Column(type: 'uuid', unique: true)]
@@ -18,8 +20,14 @@ class User
     #[ORM\Column(type: 'string', unique: true)]
     private string $username;
 
+    /**
+     * The hashed password.
+     */
     #[ORM\Column(type: 'string')]
     private string $password;
+
+    #[ORM\Column(type: 'json')]
+    private array $roles = [];
 
     public function __construct(
         string $username,
@@ -40,8 +48,39 @@ class User
         return $this->username;
     }
 
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
     public function getPassword(): string
     {
         return $this->password;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials(): void
+    {
+        // ...
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return $this->username;
     }
 }
